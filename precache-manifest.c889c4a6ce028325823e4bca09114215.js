7d5bbaf7a7f3e613879a54f3ec41acf1b0211308
J Miguel Farto <dadamahou@gmail.com>
2021-02-21 17:10:11 +0100

    Updates

---
+++ precache-manifest.c889c4a6ce028325823e4bca09114215.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fa88cc8c06d9883142e7260d5a7a386",
+    "url": "/telemath/index.html"
+  },
+  {
+    "revision": "dd00a9ec393b4d0100c6",
+    "url": "/telemath/static/css/0.52c23fd6.chunk.css"
+  },
+  {
+    "revision": "1fe2b614d96b673cd7c2",
+    "url": "/telemath/static/css/3.8462b3b8.chunk.css"
+  },
+  {
+    "revision": "b2e2233ec48d0eda9fc1",
+    "url": "/telemath/static/css/5.c61ffb77.chunk.css"
+  },
+  {
+    "revision": "962f7a1846699ba9eae4",
+    "url": "/telemath/static/css/7.792a87e0.chunk.css"
+  },
+  {
+    "revision": "62a7597dd54436a3a1bc",
+    "url": "/telemath/static/css/main.912673cd.chunk.css"
+  },
+  {
+    "revision": "dd00a9ec393b4d0100c6",
+    "url": "/telemath/static/js/0.9c1cfea8.chunk.js"
+  },
+  {
+    "revision": "1fe2b614d96b673cd7c2",
+    "url": "/telemath/static/js/3.96ae18b0.chunk.js"
+  },
+  {
+    "revision": "d8f23031687976a9a77d",
+    "url": "/telemath/static/js/4.1f2719b6.chunk.js"
+  },
+  {
+    "revision": "b2e2233ec48d0eda9fc1",
+    "url": "/telemath/static/js/5.4438c625.chunk.js"
+  },
+  {
+    "revision": "04e630c062f6043530b6",
+    "url": "/telemath/static/js/6.590c485e.chunk.js"
+  },
+  {
+    "revision": "962f7a1846699ba9eae4",
+    "url": "/telemath/static/js/7.ca14dc27.chunk.js"
+  },
+  {
+    "revision": "62a7597dd54436a3a1bc",
+    "url": "/telemath/static/js/main.24e0c8c6.chunk.js"
+  },
+  {
+    "revision": "3ba5532e8865634833a5",
+    "url": "/telemath/static/js/runtime~main.067a7eb9.js"
+  },
+  {
+    "revision": "85479cc199de7ace1a44d901720601b8",
+    "url": "/telemath/static/media/bubble-tail-left.85479cc1.svg"
+  },
+  {
+    "revision": "bcf112461b322d3b888e4eb550e3386a",
+    "url": "/telemath/static/media/camomile_blurred.bcf11246.jpg"
+  },
+  {
+    "revision": "71da74694e314a0e3a855ded564a1eaf",
+    "url": "/telemath/static/media/check.71da7469.svg"
+  },
+  {
+    "revision": "ac1331a490a116a48daa6c9f41b6db80",
+    "url": "/telemath/static/media/telegram-logo.ac1331a4.svg"
+  }
+]);
